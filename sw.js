c982b204f8af58b767d46268d9b43e0399075130
Lucas Vicente <lucasvicente@gmail.com>
2026-06-05 15:55:11 -0300

    Campers App - primera versión

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'campers-v1';
+const FILES = ['./tareas_diarias.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
